a38f79b7cb6497d5986f0468f1437081731ea99a
Akhil Ivaturi <akhilkashyap16@gmail.com>
2025-04-16 12:09:24 -0500

    Fix setup.py to include correct gcc version

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+import os
+import sysconfig
+
+# Get Python include path
+python_include = sysconfig.get_paths()['include']
 
 setup(
     name='matmul_cuda',
     ext_modules=[
-        CUDAExtension('matmul_cuda', [
-            'matmul_kernel.cu',
-        ]),
+        CUDAExtension(
+            'matmul_cuda',
+            sources=['matmul_kernel.cu'],
+            extra_compile_args={
+                'cxx': ['-O3'],
+                'nvcc': [
+                    '-O3',
+                    '-ccbin=/usr/bin/gcc-11',  # or the actual path from `which gcc` after loading module
+                    f'-I{python_include}',
+                    '--compiler-options', "'-fPIC'",
+                ]
+            }
+        )
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    cmdclass={'build_ext': BuildExtension}
 )
